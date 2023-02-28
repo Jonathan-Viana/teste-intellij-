2cd21da8806806291dbe0b0d5e34d12dc25f9fd7
Jonathan Viana <ipcjonathanviana@gmail.com>
2023-02-27 22:17:16 -0300

    criando class Livro

--- src/br/com/dio/primeiro_programa.java
+++ src/br/com/dio/primeiro_programa.java
@@ -5,10 +5,46 @@ import br.com.dio.model.Gato;
 public class primeiro_programa {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livro livro = new Livro("O senhor dos anéis", 300);
+        System.out.println(livro);
+
 
         System.out.println(gato);
       /* int a = 3;
        int b = 7;
         System.out.println("Hello World e sua resposta é: " + (a+b));*/
+    }
+}
+class Livro{
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
     }
 }
